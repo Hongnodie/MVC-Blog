6d37cdf44ce6d29b688beee0cbb1ec5327f4f037
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-04-12 11:04:10 +0930

    Config.connection coded

---
+++ Config/Connection.js
@@ -0,0 +1,29 @@
+// Introduce sequelize first
+const Sequelize = require('sequelize');
+
+// init an object to be exported to server.js
+let sequelize;
+
+// if deployed at heroku (means http://*heroku*), connect to JAWSDB_URL
+// Explained in heroku https://devcenter.heroku.com/articles/jawsdb and in sequelize https://sequelize.org/docs/v6/getting-started/
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} 
+// if deployedon local, due to test need, set up the environment variables to connect to mysql as required
+// Could understand from https://www.npmjs.com/package/sequelize-connect yet not sure if merged to sequelize
+else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        //   necessary header for mysql explained at https://dev.mysql.com/doc/dev/connector-nodejs/8.0/module-Connection.html
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+        }
+    );
+}
+
+// Export the object
+module.exports = sequelize;
